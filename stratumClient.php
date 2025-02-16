c51a5ad4caf0737e472b02cf1108071ed231ab61
Rafael Fernandes <rafamex.mex@gmail.com>
2025-02-16 02:32:14 -0300

    Adicionado mineração via pool

---
+++ stratumClient.php
@@ -0,0 +1,145 @@
+<?php
+class Work
+{
+    public $job_id;
+    public $prev_hash;
+    public $coinbase1;
+    public $coinbase2;
+    public $merkle_branch;
+    public $version;
+    public $bits;
+    public $ntime;
+    public $clean_jobs;
+
+    public $extranonce1;
+
+    public function __construct() {}
+
+    public function setWork($json)
+    {
+        $this->job_id = $json["params"][0];
+        $this->prev_hash = $json["params"][1];
+        $this->coinbase1 = $json["params"][2];
+        $this->coinbase2 = $json["params"][3];
+        $this->merkle_branch = $json["params"][4]; // Array de hashes Merkle
+        $this->version = $json["params"][5];
+        $this->bits = $json["params"][6];
+        $this->ntime = $json["params"][7];
+        $this->clean_jobs = $json["params"][8];
+        return $this;
+    }
+
+    public function setExtranonce1($extranonce1)
+    {
+        $this->extranonce1 = $extranonce1;
+    }
+}
+class StratumClient
+{
+    private $socket;
+    public $username;
+    private $password;
+    private Work $work;
+
+    public function __construct($host, $port, $username, $password)
+    {
+        $this->work = new Work();
+        $this->socket = fsockopen($host, $port, $errno, $errstr, 10);
+        if (!$this->socket) {
+            die("Erro ao conectar à pool: $errstr ($errno)\n");
+        }
+
+        $this->username = $username;
+        $this->password = $password;
+        $this->subscribe();
+        $this->authorize();
+    }
+
+    private function send($data)
+    {
+        fwrite($this->socket, json_encode($data) . "\n");
+    }
+
+    private function read()
+    {
+        return json_decode(fgets($this->socket), true);
+    }
+
+    private function subscribe()
+    {
+        $this->send(["id" => 1, "method" => "mining.subscribe", "params" => []]);
+        $resp = $this->read();
+        if ($resp["id"] == 1 && !empty($resp['result'][1])) {
+            $this->work->setExtranonce1($resp['result'][1]);
+            return $resp;
+        }
+        throw new Exception("Erro ao se inscrever na pool");
+    }
+
+    private function authorize()
+    {
+        $this->send(["id" => 2, "method" => "mining.authorize", "params" => [$this->username, $this->password]]);
+        return $this->read();
+    }
+
+    public function getWork()
+    {
+        // $data = file_get_contents('blockToMine.json');
+        // $json = json_decode($data, true);
+        // $work = $this->work->setWork($json);
+        // return $work;
+        while (!feof($this->socket)) {
+            $data = fgets($this->socket);
+            if ($data) {
+                echo "Recebido: $data\n";
+
+                if (str_contains($data, '"method":"mining.notify"')) {
+                    $json = json_decode($data, true);
+                    $work = $this->work->setWork($json);
+                    return $work;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public function submitWork($jobId, $nonce, $hash)
+    {
+        $this->send(["id" => 3, "method" => "mining.submit", "params" => [$this->username, $jobId, $nonce, $hash]]);
+        $resp = $this->read();
+        while ($resp["id"] != 3) {
+            $resp = $this->read();
+        }
+        print_r($resp);
+        return $resp;
+    }
+
+    function submit_work($job_id, $nonce, $extra_nonce, $ntime)
+    {
+        $data = [
+            "method" => "mining.submit",
+            "params" => [
+                $this->username,
+                $job_id,
+                $extra_nonce,
+                $ntime,
+                $nonce
+            ],
+            "id" => 4
+        ];
+
+        $this->send($data);
+        $resp = $this->read();
+        while ($resp["id"] != 4) {
+            $resp = $this->read();
+        }
+        print_r($resp);
+        return $resp;
+    }
+
+    public function close()
+    {
+        fclose($this->socket);
+    }
+}
---
+++ stratumMiner.php
@@ -0,0 +1,99 @@
+<?php
+
+require_once("stratumClient.php");
+
+class StratumMiner
+{
+    public StratumClient $client;
+
+    function __construct($host, $port, $username, $password)
+    {
+        $this->client = new StratumClient($host, $port, $username, $password);
+    }
+
+    function build_coinbase_tx(Work $work, $extra_nonce2)
+    {
+        $cb = $work->coinbase1 . $work->extranonce1 . $extra_nonce2 . $work->coinbase2;
+        return $cb;
+    }
+    function doubleSha256($data)
+    {
+        return hash('sha256', hash('sha256', $data, true), true);
+    }
+    function merkle_root($coinbase_tx, $merkle_branch)
+    {
+        $hash = $this->doubleSha256(hex2bin($coinbase_tx));
+
+        foreach ($merkle_branch as $branch) {
+            $hash = $this->doubleSha256($hash . hex2bin($branch));
+        }
+
+        $merkle = bin2hex($hash); // A Merkle Root precisa estar em little-endian
+        return $merkle;
+    }
+
+    function build_block_header($version, $previousblock, $merkleroot, $time, $bits, $nonce)
+    {
+        $version = implode('', array_reverse(str_split($version, 2)));
+        $previousblock = implode('', array_reverse(str_split($previousblock, 2)));
+        $merkleroot = implode('', array_reverse(str_split($merkleroot, 2)));
+        $time = implode('', array_reverse(str_split(dechex($time), 2)));
+        $bits = implode('', array_reverse(str_split($bits, 2)));
+        $nonce = implode('', array_reverse(str_split(str_pad(dechex($nonce), 8, '0', STR_PAD_LEFT), 2)));
+        $blockheader = $version . $previousblock . $merkleroot . $time . $bits . $nonce;
+        $bytes = hex2bin($blockheader);
+        $hash1 = hash('sha256', $bytes, true);
+        $hash2 = hash('sha256', $hash1, true);
+        $blockhash = bin2hex($hash2);
+        $reversed_blockhash = implode('', array_reverse(str_split($blockhash, 2)));
+        return $reversed_blockhash;
+    }
+
+    function bitstotarget($bits)
+    {
+        $exponent = hexdec(substr($bits, 0, 2));
+        $coefficient = substr($bits, 2, 8);
+        $target = str_pad(str_pad($coefficient, ($exponent * 2), '0', STR_PAD_RIGHT), 64, '0', STR_PAD_LEFT);
+        return $target;
+    }
+
+    function mine(Work $work)
+    {
+        $extra_nonce2 = bin2hex(random_bytes(4)); // Gerar um nonce aleatório
+        $coinbase_tx = $this->build_coinbase_tx($work, $extra_nonce2);
+        $merkle_root = $this->merkle_root($coinbase_tx, $work->merkle_branch);
+
+        $previousblock = implode('', array_reverse(str_split($work->prev_hash, 8)));
+        $time = hexdec($work->ntime);
+        $target = $this->bitstotarget($work->bits); // Convertendo target da pool
+        $nonce = 0;
+
+        while (true) {
+            $hash = $this->build_block_header($work->version, $previousblock, $merkle_root, $time, $work->bits, $nonce);
+            $gmphash = gmp_init($hash, 16);
+            $gmptarget = gmp_init($target, 16);
+
+            if (gmp_cmp($gmphash, $gmptarget) < 0) {
+                $nonce = str_pad(dechex($nonce), 8, '0', STR_PAD_LEFT);
+                $payload = '{"params": ["' . $this->client->username . '", "' . $work->job_id . '", "' . $extra_nonce2 . '", "' . $work->ntime . '", "' . $nonce . '"], "id": 1, "method": "mining.submit"}';
+                $response = $this->client->submit_work($work->job_id, $nonce, $extra_nonce2, $work->ntime);
+                return [$payload, $response];
+            }
+
+            $nonce++;
+
+            if ($nonce > 0xFFFFFFFF) {
+                break; // Sai do loop caso ultrapasse o limite do nonce
+            }
+        }
+
+        return false; // Nenhum hash válido encontrado
+    }
+}
+
+$miner = new StratumMiner("sha256.poolbinance.com", 443, "worker", "password"); //binance example
+$work = $miner->client->getWork();
+$result = $miner->mine($work);
+if ($result) {
+    print_r($result);
+}
